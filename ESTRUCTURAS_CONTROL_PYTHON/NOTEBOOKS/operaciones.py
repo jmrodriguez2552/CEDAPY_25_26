14b0251bfcfeae163633603e1f2bf4422660cfce
Jordi Pozo <jpozoc@gmail.com>
2026-01-21 18:07:24 +0100

    Actualización 21/01/26
    Testing

---
+++ ESTRUCTURAS_CONTROL_PYTHON/NOTEBOOKS/operaciones.py
@@ -0,0 +1,10 @@
+def suma(x, y):
+   return x + y
+def resta(x, y):
+   return x - y
+def multiplicacion(x, y):
+    return x * y
+def division(x, y):
+   if y == 0:
+        raise ValueError("No se puede dividir por cero")  
+   return x / y
---
+++ ESTRUCTURAS_CONTROL_PYTHON/NOTEBOOKS/test_prueba.py
@@ -0,0 +1,7 @@
+import pytest
+
+def test_suma_enteros():
+      x = 3
+      y = 5
+      resultado = 6
+      assert x+y == resultado
