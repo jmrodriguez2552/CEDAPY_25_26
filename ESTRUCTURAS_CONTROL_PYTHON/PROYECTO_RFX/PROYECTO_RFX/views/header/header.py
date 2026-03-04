d43f21c6960e05e7a5a92969d5db57ddebd79025
jmrodriguez2552 <jmrodriguez2552@gmail.com>
2026-03-04 21:03:33 +0100

    cambios a dia 04 de marzo

--- ESTRUCTURAS_CONTROL_PYTHON/PROYECTO_RFX/PROYECTO_RFX/views/header/header.py
+++ ESTRUCTURAS_CONTROL_PYTHON/PROYECTO_RFX/PROYECTO_RFX/views/header/header.py
@@ -3,7 +3,7 @@ import reflex as rx
 def header()-> rx.Component:
     return rx.vstack(
         rx.center(
-            rx.avatar(src="/home/jordi/Documentos/Ribera/Curso_25_26/CEDAPY/REPOS/CEDAPY_25_26/ESTRUCTURAS_CONTROL_PYTHON/PROYECTO_RFX/assets/JPC_CARTOON.jpeg", radius="medium"),
+            rx.image(src="/JPC_CARTOON.jpeg", width= "200px", height="auto"),
             rx.text("@jordipozo"),
             rx.text("""Hola me llamo Jordi Pozo.
                     Soy profesor de Ciclos Formativos
